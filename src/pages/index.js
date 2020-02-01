35d167310539f016af88fab2a0d4a3f5cce8c9b6
Iseeumhmm <rickheffren@Ricks-MacBook-Pro.local>
2020-02-01 18:46:49 -0500

    under construction page

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ import LoadingPage from '../components/spashScreen'
 import instagram from '../images/icons/instagram.png'
 import facebook from '../images/icons/facebook.png'
 import styled, {keyframes} from 'styled-components'
+import logo from '../images/logos/LargeLogo.png'
 
 // Styles
 
@@ -40,7 +41,27 @@ const FireFoxHome = styled.div`
   background-size: cover;
   background-position: center center;
 `
+const ConstructionPage = styled.div`
+display: flex;
+flex-flow: column nowrap;
+align-items: center;
+text-align: center;
+width: 100%;
+height: 100vh;
+background-image: url('http://images.ctfassets.net/ur1u6ni4z1cg/27XoXJe8sqHgerkKcPd41w/98b1cf252e9c5aa2196918471bbb714e/pool3edited.jpg?w=1920&q=40&fl=progressive');
+background-size: cover;
+background-position: bottom center;
+text-shadow: 5px 5px #0000;
+img {
+  width: 23rem;
+  left: 50%;
+}
+h1 { 
+  color: #FFF;
+  text-align: center;
+   }
 
+`
 function Home() {
   const { homePageSliderImages } = useRouteData()
   const [viewHeight, setViewHeight] = useState(0)
@@ -110,7 +131,20 @@ function Home() {
     </Container>
   )
 
-  return loading ? <LoadingPage/> : page
+  let underConstruction = (
+    <ConstructionPage>
+      <div>
+        <img src={logo} alt="logo" />
+        <h1>We're currently making some changes to our site.<br /><br />
+        Sorry for the inconvenience.<br /><br />
+        We can still be reached at <br />
+        </h1>
+        <h1 className="vince_phone">(519) 403-5699</h1>
+      </div>
+    </ ConstructionPage>
+  )
+  return underConstruction
+  // return loading ? <LoadingPage/> : page
 }
 
 export default Home
