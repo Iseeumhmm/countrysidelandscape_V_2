16589f268330fd7a76f99d2618651d32b7291b7b
Iseeumhmm <rickheffren@Ricks-MacBook-Pro.local>
2020-02-01 19:01:47 -0500

    static.config

--- static.config.js
+++ static.config.js
@@ -119,7 +119,7 @@ export default {
       // },
     ]
   },
-  // siteRoot: 'https://countryside.rickheffren.com',
+  siteRoot: 'https://countryside.rickheffren.com',
   webpack: (config, { stage }) => {
     if (stage === 'prod') {
       config.entry = ['babel-polyfill', config.entry]
